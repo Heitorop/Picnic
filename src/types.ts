37a55b1a484b93f657d224d38d659d263545cd5c
Dold <dold@dt.team>
2024-04-15 12:42:12 +0300

    fix types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,30 @@
+
+// COLOR
 type RGB = `rgb(${number}, ${number}, ${number})`;
 type RGBA = `rgba(${number}, ${number}, ${number}, ${number})`;
 type HEX = `#${string}`;
 
 export type Color = RGB | RGBA | HEX;
+
+// Anchor
+
+const block = ["top", "bottom"] as const;
+const inline = ["start", "end", "left", "right"] as const;
+type Tblock = (typeof block)[number];
+type Tinline = (typeof inline)[number];
+export type Anchor =
+  | Tblock
+  | Tinline
+  | "center"
+  | "center center"
+  | `${Tblock} ${Tinline | "center"}`
+  | `${Tinline} ${Tblock | "center"}`;
+
+  // MENU
+
+  export interface Menu {
+    activator: string;
+    text: string;
+    location: Anchor;
+    origin: Anchor;
+  }
