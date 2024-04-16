183a516458146fdc3c3d9d5e88b42b5c0425c040
Dold <dold@dt.team>
2024-04-16 11:38:27 +0300

    add firebase

---
+++ src/plugins/firebase.ts
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCb8PkeBY2aXwVCRXV444a6aowjwtAoqXo",
+  authDomain: "vue-auth-95a6b.firebaseapp.com",
+  projectId: "vue-auth-95a6b",
+  storageBucket: "vue-auth-95a6b.appspot.com",
+  messagingSenderId: "732525788667",
+  appId: "1:732525788667:web:c02a808a07b983c3e35f5e"
+};
